8df5c2c9543f895fbc0d1f8c3902c0f539af6820
buyfakett <jhj269012469@qq.com>
2023-06-10 22:52:12 +0800

    修复自定义local nginx bug

--- initialization_local.sh
+++ initialization_local.sh
@@ -176,8 +176,6 @@ function install_local_nginx(){
         cp download_file/reload_local.sh ${loacl_nginx_site}/conf/conf.d/reload.sh
         cp download_file/local_nginx_index.conf ${loacl_nginx_site}/conf/conf.d/index.conf
 
-        sed -i "s/access_log \/root\/nginx\/logs\/nginx.log main;/access_log \${loacl_nginx_site}\/logs\/nginx.log main;/g" /usr/local/openresty/nginx/conf/nginx.conf
-
         cat << EOF > ${loacl_nginx_site}/nginx_log.sh
 #!/bin/bash
 now_date=\`date -d '-1 day' +%Y-%m-%d\`
