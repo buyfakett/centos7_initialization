9e7ef1994dd78819f9e35654149ec59c1fb09e04
buyfakett <buyfakett@vip.qq.com>
2023-08-18 09:33:18 +0800

    update: beta time

--- initialization.sh
+++ initialization.sh
@@ -6,7 +6,7 @@
 # * Author        : buyfakett
 # * Email         : buyfakett@vip.qq.com
 # * Create time   : 2023-1-28
-# * Last modified : 2023-8-17
+# * Last modified : 2023-8-18
 # * Filename      : initialization.sh
 # * Description   : shell
 # **********************************************************
