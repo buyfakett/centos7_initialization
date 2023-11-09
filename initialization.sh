70b333a12599e154fd38f2384a8a59b60284014a
buyfakett <buyfakett@vip.qq.com>
2023-11-09 20:08:48 +0800

    :sparkles: update: beta time

--- initialization.sh
+++ initialization.sh
@@ -6,7 +6,7 @@
 # * Author        : buyfakett
 # * Email         : buyfakett@vip.qq.com
 # * Create time   : 2023-1-28
-# * Last modified : 2023-10-21
+# * Last modified : 2023-11-9
 # * Filename      : initialization.sh
 # * Description   : shell
 # **********************************************************
